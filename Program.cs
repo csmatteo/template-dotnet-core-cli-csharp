126332cabb97a72fefec211b68987d566aee92ae
csmatteo <64636007+csmatteo@users.noreply.github.com>
2023-03-17 10:06:55 +0000

    code training

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 namespace dotnetcore
 {
@@ -6,7 +9,36 @@ namespace dotnetcore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            String secretName = "MySecret";
+            String keyVaultName = "training-no-purge-prot";
+            var keyvalueUri = "https://training-no-purge-prot.vault.azure.net";
+            SecretClientOptions options = new SecretClientOptions(){
+                Retry = {
+                    Delay = TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential
+                }
+            };
+            var client = new SecretClient(new Uri(keyvalueUri), new DefaultAzureCredential(), options);
+            KeyVaultSecret secret = client.GetSecret(secretName);
+
+            Console.WriteLine("GetSecret:" + secret.Value);
+            Console.Write("Enter Secret > ");
+
+            String secretValue = Console.ReadLine();
+            client.SetSecret(secretName, secretValue);
+            Console.Write("SetSecret:");
+            Console.Write("\tKey: " + secretName);
+            Console.Write("\tValue: " + secretValue);
+
+            Console.WriteLine("GetSecret: " +secret.Value);
+
+            client.StartDeleteSecret(secretName);
+            Console.WriteLine("StartDeleteSecret: " + keyVaultName);
+
+            Console.WriteLine("GetSecret: " +secret.Value);
         }
     }
 }
